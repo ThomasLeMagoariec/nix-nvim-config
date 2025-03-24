e49e5995af4c75eb322f63ec22dd824e25bb881a
thomas <thomas.lemagoariec@outlook.com>
2025-03-24 11:17:08 +0100

    auto

--- config/lua/neo-tree.lua
+++ config/lua/neo-tree.lua
@@ -1,4 +1,4 @@
-local neotree = require("neo-tree-nvim")
+#local neotree = require("neo-tree-nvim")
 local opt = { noremap = true }
 
-vim.api.nvim_set_keymap("n", "<leader>fe", ":Neotree", opt)
+vim.api.nvim_set_keymap("n", "<leader>fe", ":Neotree filesystem reveal left<CR>", opt)

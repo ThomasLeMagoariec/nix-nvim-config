228cbf6896eb2665cccea72ab3349340184a8b1e
thomas <thomas.lemagoariec@outlook.com>
2025-03-25 21:51:52 +0100

    auto

--- config/keybindings.nix
+++ config/keybindings.nix
@@ -1,6 +1,6 @@
 {self, ...}: {
     keymaps = [
         { key = "<leader>ca"; action = "<cmd>lua vim.lsp.buf.code_action()<CR>"; }
-        { key = "<leader>fe"; action = "<cmd>Neotree filesystem reveal left<CR>"; }
+        { key = "<leader>fe"; action = "<cmd>Neotree filesystem reveal float<CR>"; }
     ];
 }

d4ace00079e9e8b6496ddfb9a2243235714749c8
thomas <thomas.lemagoariec@outlook.com>
2025-03-25 18:09:46 +0100

    auto

--- config/default.nix
+++ config/default.nix
@@ -25,18 +25,6 @@
                 lua_ls.enable = true;
             };
 
-            luaConfig.post = ''
-nnoremap <silent> gd <cmd>lua vim.lsp.buf.definition()<CR>\n
-nnoremap <silent> K <cmd>lua vim.lsp.buf.hover()<CR>\n
-nnoremap <silent> <leader>rn <cmd>lua vim.lsp.buf.rename()<CR>\n
-nnoremap <silent> <leader>ca <cmd>lua vim.lsp.buf.code_action()<CR>\n
-nnoremap <silent> gr <cmd>lua vim.lsp.buf.references()<CR>\n
-nnoremap <silent> <leader>d <cmd>lua vim.diagnostic.open_float()<CR>\n
-nnoremap <silent> [d <cmd>lua vim.diagnostic.goto_prev()<CR>\n
-nnoremap <silent> ]d <cmd>lua vim.diagnostic.goto_next()<CR>\n
-nnoremap <silent> <C-k> <cmd>lua vim.lsp.buf.signature_help()<CR>\n
-nnoremap <silent> <leader>f <cmd>lua vim.lsp.buf.formatting()<CR>\n
-            '';
         };
     };
 
@@ -45,5 +33,6 @@ nnoremap <silent> <leader>f <cmd>lua vim.lsp.buf.formatting()<CR>\n
             action = "<cmd>help<CR>";
             key = "<leader>g";
         }
+        { key = "<leader>gd"; action = "<cmd>lua vim.lsp.buf.definition()<CR>"}
     ];
 }

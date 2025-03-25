65cf19313d6636c74e57b7e8a1420e5e2bd6af1c
thomas <thomas.lemagoariec@outlook.com>
2025-03-25 18:06:09 +0100

    auto

--- config/default.nix
+++ config/default.nix
@@ -26,16 +26,16 @@
             };
 
             luaConfig.post = ''
-                nnoremap <silent> gd <cmd>lua vim.lsp.buf.definition()<CR>
-                nnoremap <silent> K <cmd>lua vim.lsp.buf.hover()<CR>
-                nnoremap <silent> <leader>rn <cmd>lua vim.lsp.buf.rename()<CR>
-                nnoremap <silent> <leader>ca <cmd>lua vim.lsp.buf.code_action()<CR>
-                nnoremap <silent> gr <cmd>lua vim.lsp.buf.references()<CR>
-                nnoremap <silent> <leader>d <cmd>lua vim.diagnostic.open_float()<CR>
-                nnoremap <silent> [d <cmd>lua vim.diagnostic.goto_prev()<CR>
-                nnoremap <silent> ]d <cmd>lua vim.diagnostic.goto_next()<CR>
-                nnoremap <silent> <C-k> <cmd>lua vim.lsp.buf.signature_help()<CR>
-                nnoremap <silent> <leader>f <cmd>lua vim.lsp.buf.formatting()<CR>
+nnoremap <silent> gd <cmd>lua vim.lsp.buf.definition()<CR>
+nnoremap <silent> K <cmd>lua vim.lsp.buf.hover()<CR>
+nnoremap <silent> <leader>rn <cmd>lua vim.lsp.buf.rename()<CR>
+nnoremap <silent> <leader>ca <cmd>lua vim.lsp.buf.code_action()<CR>
+nnoremap <silent> gr <cmd>lua vim.lsp.buf.references()<CR>
+nnoremap <silent> <leader>d <cmd>lua vim.diagnostic.open_float()<CR>
+nnoremap <silent> [d <cmd>lua vim.diagnostic.goto_prev()<CR>
+nnoremap <silent> ]d <cmd>lua vim.diagnostic.goto_next()<CR>
+nnoremap <silent> <C-k> <cmd>lua vim.lsp.buf.signature_help()<CR>
+nnoremap <silent> <leader>f <cmd>lua vim.lsp.buf.formatting()<CR>
             '';
         };
     };

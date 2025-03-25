a6854c054a6c086a902debe6d6cf9281582b638c
thomas <thomas.lemagoariec@outlook.com>
2025-03-25 17:45:39 +0100

    auto

--- config/default.nix
+++ config/default.nix
@@ -5,44 +5,20 @@
 
     plugins = {
         lualine.enable = true;
-    };
 
-    plugins.lsp = {
-        enable = true;
-        servers = {
-            lua_ls.enable = true;
+        cmp = {
+            enable = true;
+            autoEnableSources = false;
+            settings.sources = [
+                { name = "nvim_lsp"; }
+                { name = "path"; }
+                { name = "buffer"; }
+            ];
         };
-    };
 
-    plugins.nvim-cmp = {
-        enable = true;
-        autoEnableSources = true;
-        sources = [
-            { name = "nvim_lsp"; }
-            { name = "path"; }
-            { name = "buffer"; }
-        ];
-    };
-
-    mapping = {
-        "<CR>" = "cmp.mapping.confirm({ select = true })";
-        "<Tab>" = {
-            action = ''
-                function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    elseif luasnip.expandable() then
-                        luasnip.expand()
-                    elseif luasnip.expand_or_jumpable() then
-                        luasnip.expand_or_jump()
-                    else if check_backspace() then
-                        fallback()
-                    else
-                        fallback()
-                    end
-                end
-            '';
-            modes = [ "i" "s" ];
+        lsp = {
+            enable = true;
+            autoLoad = true;
         };
     };
 }

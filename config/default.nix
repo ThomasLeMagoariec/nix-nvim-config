46404c6b8073c4510e912bd3b601468b7ae6b0b6
thomas <thomas.lemagoariec@outlook.com>
2025-03-25 18:18:38 +0100

    auto

--- config/default.nix
+++ config/default.nix
@@ -29,15 +29,15 @@
     };
 
     keymaps = [
-        { key = "gd" action = "<cmd>vim.lsp.buf.definition()<CR>" }
-        { key = "K" action = "<cmd>vim.lsp.buf.hover()<CR>" }
-        { key = "<leader>rn" action = "<cmd>vim.lsp.buf.rename()<CR>" }
-        { key = "<leader>ca" action = "<cmd>vim.lsp.buf.code_action()<CR>" }
-        { key = "gr" action = "<cmd>vim.lsp.buf.references()<CR>" }
-        { key = "<leader>d" action = "<cmd>vim.diagnostic.open_float()<CR>" }
-        { key = "[d" action = "<cmd>vim.diagnostic.goto_prev()<CR>" }
-        { key = "]d" action = "<cmd>vim.diagnostic.goto_next()<CR>" }
-        { key = "<C-k>" action = "<cmd>vim.lsp.buf.signature_help()<CR>" }
-        { key = "<leader>f" action = "<cmd>vim.lsp.buf.formatting()<CR>" }
+        { key = "gd" action = "<cmd>vim.lsp.buf.definition()<CR>"; }
+        { key = "K" action = "<cmd>vim.lsp.buf.hover()<CR>"; }
+        { key = "<leader>rn" action = "<cmd>vim.lsp.buf.rename()<CR>"; }
+        { key = "<leader>ca" action = "<cmd>vim.lsp.buf.code_action()<CR>"; }
+        { key = "gr" action = "<cmd>vim.lsp.buf.references()<CR>"; }
+        { key = "<leader>d" action = "<cmd>vim.diagnostic.open_float()<CR>"; }
+        { key = "[d" action = "<cmd>vim.diagnostic.goto_prev()<CR>"; }
+        { key = "]d" action = "<cmd>vim.diagnostic.goto_next()<CR>"; }
+        { key = "<C-k>" action = "<cmd>vim.lsp.buf.signature_help()<CR>"; }
+        { key = "<leader>f" action = "<cmd>vim.lsp.buf.formatting()<CR>"; }
     ];
 }

a4d6101a2e157b6e4b159461a6f7db3f6a800677
thomas <thomas.lemagoariec@outlook.com>
2025-03-25 19:27:31 +0100

    auto

--- config/plugins.nix
+++ config/plugins.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
     plugins = {
         lualine.enable = true;
+        luasnip.enable = true;
         cmp = {
             enable = true;
             autoEnableSources = true;
@@ -11,53 +12,55 @@
             ];
 
             luaConfig.post = ''
-                local has_words_before = function()
-  unpack = unpack or table.unpack
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
+local luasnip = require("luasnip")
+local cmp = require("cmp")
 
-local feedkey = function(key, mode)
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-end
-
-local cmp = require('cmp')
-cmp.setup {
+cmp.setup({
 
   -- ... Your other configuration ...
 
   mapping = {
 
     -- ... Your other mappings ...
+   ['<CR>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            if luasnip.expandable() then
+                luasnip.expand()
+            else
+                cmp.confirm({
+                    select = true,
+                })
+            end
+        else
+            fallback()
+        end
+    end),
 
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif vim.fn["vsnip#available"](1) == 1 then
-        feedkey("<Plug>(vsnip-expand-or-jump)", "")
-      elseif has_words_before() then
-        cmp.complete()
+      elseif luasnip.locally_jumpable(1) then
+        luasnip.jump(1)
       else
-        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+        fallback()
       end
     end, { "i", "s" }),
 
-    ["<S-Tab>"] = cmp.mapping(function()
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-        feedkey("<Plug>(vsnip-jump-prev)", "")
+      elseif luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
       end
     end, { "i", "s" }),
 
     -- ... Your other mappings ...
-
-  }
+  },
 
   -- ... Your other configuration ...
-
-}
-            '';
+})            '';
 
         };
 

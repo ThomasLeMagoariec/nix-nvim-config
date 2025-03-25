02c3465663d307e1a2cfe0dab3b2dc33237e2d49
thomas <thomas.lemagoariec@outlook.com>
2025-03-25 19:16:17 +0100

    auto

--- config/plugins.nix
+++ config/plugins.nix
@@ -1,6 +1,22 @@
 { pkgs, ... }: {
     plugins = {
         lualine.enable = true;
-    
+        cmp = {
+            autoEnableSources = true;
+            settings.sources = [
+                { name = "nvim_lsp"; }
+                { name = "path"; }
+                { name = "buffer"; }
+            ];
+        };
+
+        cmp-nvim-lsp.enable = true;
+
+        lsp = {
+            enable = true;
+            servers = {
+                lua_ls.enable = true;
+            };
+        };
     };
 }
